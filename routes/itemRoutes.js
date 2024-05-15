242d165c729652f233c07aa48dcd6eff924b5eaf
Ricardo Perez <rxperez12@gmail.com>
2024-05-15 16:22:55 -0700

    added patch and get /:name

--- routes/itemRoutes.js
+++ routes/itemRoutes.js
@@ -1,7 +1,27 @@
 import express from "express";
 import items from "fakeDb.js";
+import { NotFoundError, BadRequestError } from "../expressError";
 
 const router = new express.Router();
+
+
+/** Take variable and return a single item from shopping list if it exists
+ * Input:
+ *  /:name
+ * Output:
+ *  {added: {name: "popsicle", price: 1.45}}
+*/
+router.get("/:name", function (req, res) {
+  const itemName = req.params.name;
+  const item = items.filter(item => item.name === itemName)[0];
+
+  if (item !== undefined) {
+    return res.json({ item });
+  } else {
+    throw new NotFoundError();
+  }
+});
+
 
 /** Returns list of shopping items
  * Eg. {
@@ -12,20 +32,41 @@ const router = new express.Router();
         }
 */
 router.get("", function (req, res) {
-    return res.json({ items });
+  return res.json({ items });
 });
 
 
-/** Returns list of shopping items
- * Eg. {
- *          added: {
-                { name: "popsicle", price: 1.45 },
-                { name: "cheerios", price: 3.40 }
-            }
-        }
+/** Adds shopping item to list. Returns JSON of shopping list item added
+ * Input:
+ *  {name: "popsicle", price: 1.45}
+ * Output:
+ *  {added: {name: "popsicle", price: 1.45}}
 */
 router.post("", function (req, res) {
-    const newItem = req.body;
-    items.push(newItem);
-    return res.json({ added: newItem });
+  const newItem = req.body;
+  items.push(newItem);
+  return res.json({ added: newItem });
+});
+
+
+
+/** Take name of item and JSON and update existing item
+ * Input:
+ *  /:name
+ *  body: {name: "new popsicle", price: 2.45}
+ * Output:
+ *  {updated: {name: "new popsicle", price: 2.45}}
+*/
+router.patch("/:name", function (req, res) {
+  const itemEdits = req.body;
+  const item = items.filter(item => item.name === itemName)[0];
+
+  if (item !== undefined) {
+    item['name'] = itemEdits['name'] || item['name'];
+    item['price'] = itemEdits['price'] || item['price'];
+
+    return res.json({ updated: item });
+  } else {
+    throw new NotFoundError();
+  }
 });
